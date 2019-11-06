dc51048f9a3d763509528960db05733864b63482
Nick <nsousanis14@yahoo.com>
2019-11-05 20:40:12 -0600

    Actually entered stuff into the database this time

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  # EXAMPLE HTML ROUTE
-  # get "/photos" => "photos#index"
-
-  # EXAMPLE JSON ROUTE WITH API NAMESPACE
-  # namespace :api do
-  #   get "/photos" => "photos#index"
-  # end
+  namespace :api do
+    get "/all_products_url" => "products#all_products_method"
+    get "/product_one_url" => 
+    get "/product_two_url"
+    get "/product_three_url"
+    get "/"
+  end
 end
