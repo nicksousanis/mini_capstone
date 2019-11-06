36bcefaf14a7bb3b57fb48634d9e44ef16424e6c
Nick <nsousanis14@yahoo.com>
2019-11-05 20:46:57 -0600

    Added route to view ALL products and each product individually

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -3,4 +3,29 @@ class Api::ProductsController < ApplicationController
     @products = Product.all
     render "all_products.json.jb"
   end
+
+  def product_one_method
+    @product_one = Product.first
+    render "first_product.json.jb"
+  end
+
+  def product_two_method
+    @product_two = Product.second
+    render "second_product.json.jb"
+  end
+
+  def product_three_method
+    @product_three = Product.third
+    render "third_product.json.jb"
+  end
+
+  def product_four_method
+    @product_four = Product.fourth
+    render "fourth_product.json.jb"
+  end
+
+  def product_five_method
+    @product_five = Product.fifth
+    render "fifth_product.json.jb"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   namespace :api do
     get "/all_products_url" => "products#all_products_method"
-    get "/product_one_url" => 
-    get "/product_two_url"
-    get "/product_three_url"
-    get "/"
+    get "/product_one_url" => "products#product_one_method"
+    get "/product_two_url" => "products#product_two_method"
+    get "/product_three_url" => "products#product_three_method"
+    get "/product_four_url" => "products#product_four_method"
+    get "/prudct_five_url" => "products#product_five_method"
   end
 end
