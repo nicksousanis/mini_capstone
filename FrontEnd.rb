e3de149db0337b69c8932ef53f4a39136afe146f
Nick <nsousanis14@yahoo.com>
2019-11-10 08:59:23 -0600

    Added post request to FrontEnd

--- FrontEnd.rb
+++ FrontEnd.rb
@@ -2,25 +2,36 @@ require "http"
 require "tty-table"
 require "tty-prompt"
 
-puts "Please enter 'all' if you would like to see all products or 'individual' if you would like to see a specific one:"
-while true
-  input = gets.chomp
-  if input == "all"
-    response = HTTP.get("http://localhost:3000/api/products")
-    data = response.parse
-    pp data
-    break
-  elsif input == "individual"
-    puts "Please enter the product id:"
-    product_id = gets.chomp
-    response = HTTP.get("http://localhost:3000/api/products/#{product_id}")
-    data = response.parse
-    pp data
-    break
-  else
-    puts "That is not a valid response, please enter 'all' or 'individual'"
-  end
-end
+puts "Enter the new product you would like to add to the list. Make sure to include name, price, image_url, and description."
+name = gets.chomp
+price = gets.chomp.to_i
+image_url = gets.chomp
+description = gets.chomp
+
+response = HTTP.post("http://localhost:3000/api/products", :params => { name: name, price: price, image_url: image_url, description: description })
+
+data = response.parse
+pp data
+
+# puts "Please enter 'all' if you would like to see all products or 'individual' if you would like to see a specific one:"
+# while true
+#   input = gets.chomp
+#   if input == "all"
+#     response = HTTP.get("http://localhost:3000/api/products")
+#     data = response.parse
+#     pp data
+#     break
+#   elsif input == "individual"
+#     puts "Please enter the product id:"
+#     product_id = gets.chomp
+#     response = HTTP.get("http://localhost:3000/api/products/#{product_id}")
+#     data = response.parse
+#     pp data
+#     break
+#   else
+#     puts "That is not a valid response, please enter 'all' or 'individual'"
+#   end
+# end
 # table = TTY::Table.new ["name", "price", "image_url" "description"], ["test", "test", "test", "test"]
 # table.render(:basic)
 # table = TTY::Table.new ["header1", "header2"], [["a1", "a2"], ["b1", "b2"]]
--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -1,12 +1,23 @@
 class Api::ProductsController < ApplicationController
   def index
     @products = Product.all
-    render "all_products.json.jb"
+    render "index.json.jb"
   end
 
   def show
     @current_product = params["id"].to_i
     @test = Product.find_by(id: "#{@current_product}")
-    render "product_query.json.jb"
+    render "show.json.jb"
+  end
+
+  def create
+    @product = Product.new(
+      name: params[:name],
+      price: params[:price],
+      image_url: params[:image_url],
+      description: params[:description],
+    )
+    @product.save
+    render "create.json.jb"
   end
 end
