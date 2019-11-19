eb7ae02d2b3376f531a7ef31fdbb5f1e0a56990e
Nick <nsousanis14@yahoo.com>
2019-11-18 20:51:32 -0600

    Added user signup action

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do
+    post "/users" => "users#create"
+
     get "/products" => "products#index"
     get "/products/:id" => "products#show"
     post "/products" => "products#create"
